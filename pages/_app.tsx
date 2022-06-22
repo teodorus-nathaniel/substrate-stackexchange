ab4546936f5e48fa14c223d0b0422794f24fabd6
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-06-22 20:42:13 +0700

    Create connect wallet button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@talisman-connect/components/talisman-connect-components.esm.css'
+import '@talisman-connect/ui/talisman-connect-ui.esm.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next'
+import Button from '../components/Button'
+import ConnectWalletButton from '../components/ConnectWalletButton'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className='text-center text-2xl font-bold'>Welcome to Substrate StackExchange!</h1>
+      <h1 className='text-center text-2xl font-bold'>
+        Welcome to Substrate StackExchange!
+      </h1>
       <h2 className='text-center text-lg'>Powered by Subsocial</h2>
+      <ConnectWalletButton />
     </div>
   )
 }
