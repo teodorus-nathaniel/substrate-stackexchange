2d899606d6643147a5e8588430ef27b9ee6ea7b6
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-06-23 13:41:09 +0700

    Add multiple post inside new questions page

--- src/pages/questions/new.tsx
+++ src/pages/questions/new.tsx
@@ -3,7 +3,11 @@ import Post from '#/containers/Post'
 export default function NewQuestionsPage() {
   return (
     <div>
-      <Post />
+      <div className='flex flex-col space-y-8'>
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   )
 }
