e59bbf3053a93d509f84cf58c70870dfdae9f03f
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-06-23 11:42:11 +0700

    Refactor Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,37 +2,35 @@ import { hoverRingClassName } from '#/lib/constants/common-classnames'
 import clsx from 'clsx'
 import { forwardRef, HTMLProps } from 'react'
 
-type Variants = 'filled' | 'outlined'
-type Colors = 'brand' | 'secondary'
-type Styles = `${Variants}-${Colors}`
-
-const styles: Record<Styles, string> = {
+const variants = {
   'filled-brand': clsx(
     hoverRingClassName,
     'bg-brand text-white',
     'enabled:active:brightness-90 enabled:active:bg-brand',
     'disabled:opacity-75'
   ),
-  'filled-secondary': clsx(),
-  'outlined-brand': clsx(),
-  'outlined-secondary': clsx()
+  unstyled: clsx('hover:bg-gray-700', 'active:bg-gray-800')
 }
 
-interface Props extends HTMLProps<HTMLButtonElement> {
+const sizes = {
+  large: clsx('px-12 py-2'),
+  medium: clsx('px-6 py-2'),
+  small: clsx('px-3 py-1')
+}
+
+interface Props extends Omit<HTMLProps<HTMLButtonElement>, 'size'> {
   children: any
   className?: string
-  variant?: Variants
-  color?: Colors
-  largeHorizontalPadding?: boolean
+  size?: keyof typeof sizes
+  variant?: keyof typeof variants
 }
 
 const Button = forwardRef<HTMLButtonElement, Props>(function Button(
   {
     children,
     className,
-    variant = 'filled',
-    color = 'brand',
-    largeHorizontalPadding = false,
+    variant = 'filled-brand',
+    size = 'medium',
     type,
     ...buttonProps
   },
@@ -40,12 +38,12 @@ const Button = forwardRef<HTMLButtonElement, Props>(function Button(
 ) {
   const classNames = clsx(
     'flex justify-center items-center',
-    `py-2 ${largeHorizontalPadding ? 'px-12' : 'px-6'}`,
     'rounded-md font-bold',
     'cursor-pointer disabled:cursor-not-allowed',
     'transition-colors ease-out',
     'enabled:active:translate-y-px',
-    styles[`${variant}-${color}`],
+    sizes[size],
+    variants[variant],
     className
   )
 
