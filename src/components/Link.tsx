0ff260ef14f75e33e2b8718d97e4eb977dab00c9
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-06-23 11:42:19 +0700

    Add variant for Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,15 +2,30 @@ import clsx from 'clsx'
 import RouterLink from 'next/link'
 import { forwardRef, HTMLProps } from 'react'
 
+const variants = {
+  default: clsx(),
+  primary: clsx(
+    'text-blue-400',
+    'hover:text-blue-300 hover:underline focus:text-blue-300 focus:underline',
+    'active:text-blue-500 visited:text-purple-500'
+  )
+}
+
 export interface LinkProps extends HTMLProps<HTMLAnchorElement> {
   ref?: any
+  variant?: keyof typeof variants
 }
 
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(
-  { className, href = '', ...anchorProps },
+  { className, href = '', variant = 'default', ...anchorProps },
   ref
 ) {
-  const classes = clsx('relative cursor-pointer', className)
+  const classes = clsx(
+    'relative cursor-pointer',
+    'transition duration-150',
+    variants[variant],
+    className
+  )
   return href ? (
     <RouterLink href={href} passHref>
       <a {...anchorProps} ref={ref} className={classes} />
