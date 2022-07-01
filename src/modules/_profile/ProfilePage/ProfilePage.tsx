ff01ee88a5fdfdb67056f80170db24c8eda6cc55
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-07-01 20:52:11 +0700

    Remove dummy in profile page

--- src/modules/_profile/ProfilePage/ProfilePage.tsx
+++ src/modules/_profile/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,3 @@
-import RichTextArea from '#/components/inputs/RichTextArea'
 import TabLayout, { TabData } from '#/components/TabLayout'
 import TwoColumnsPageLayout from '#/containers/layouts/TwoColumnsPageLayout'
 import clsx from 'clsx'
@@ -15,41 +14,12 @@ export default function ProfilePage() {
   return (
     <TwoColumnsPageLayout
       leftSection={
-        <>
-          <TabLayout
-            className={clsx('w-full text-sm')}
-            tabs={tabs}
-            setSelectedTab={setSelectedTab}
-            selectedTab={selectedTab}
-          />
-          <RichTextArea name='test' />
-          <div className={clsx('text-4xl')}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              dolorum quis aspernatur ipsum dolore explicabo quod perferendis,
-              repudiandae error provident voluptates! Vero illo ducimus
-              voluptatum laborum vitae officia non similique!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              dolorum quis aspernatur ipsum dolore explicabo quod perferendis,
-              repudiandae error provident voluptates! Vero illo ducimus
-              voluptatum laborum vitae officia non similique!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              dolorum quis aspernatur ipsum dolore explicabo quod perferendis,
-              repudiandae error provident voluptates! Vero illo ducimus
-              voluptatum laborum vitae officia non similique!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              dolorum quis aspernatur ipsum dolore explicabo quod perferendis,
-              repudiandae error provident voluptates! Vero illo ducimus
-              voluptatum laborum vitae officia non similique!
-            </p>
-          </div>
-        </>
+        <TabLayout
+          className={clsx('w-full text-sm')}
+          tabs={tabs}
+          setSelectedTab={setSelectedTab}
+          selectedTab={selectedTab}
+        />
       }
       rightSection={<ProfileSection />}
     />
