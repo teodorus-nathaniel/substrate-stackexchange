5e841a9ab1d11cfbeade7f61164afefa2456008e
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-07-02 17:35:47 +0700

    Add answer form in question detail

--- src/modules/_questions/QuestionDetailPage/QuestionDetailSection.tsx
+++ src/modules/_questions/QuestionDetailPage/QuestionDetailSection.tsx
@@ -1,3 +1,5 @@
+import Button from '#/components/Button'
+import RichTextArea from '#/components/inputs/RichTextArea'
 import { useIntegratedSkeleton } from '#/components/SkeletonFallback'
 import PostDetail from '#/containers/PostDetail'
 import { useGetQuestion } from '#/services/subsocial/queries'
@@ -44,13 +46,23 @@ export default function QuestionDetailSection() {
         post={question}
         withBorderBottom
         isLoading={loadingChecker(question)}
-        // isLoading
       />
       <div className={clsx('flex flex-col', 'space-y-8')}>
         <p className={clsx('text-xl font-bold', 'mt-4')}>4 Answers</p>
         <PostDetail post={dummyAns} withBorderBottom />
-        <PostDetail post={dummyAns1} />
+        <PostDetail post={dummyAns1} withBorderBottom />
       </div>
+      <form className={clsx('flex flex-col', 'mt-8')}>
+        <p className={clsx('text-xl font-bold', 'mb-1')}>Your Answer</p>
+        <p className={clsx('text-sm text-text-secondary', 'mb-4')}>
+          Please be sure to answer the question. Provide details and share your
+          research!
+        </p>
+        <RichTextArea name={`answer-${id}`} />
+        <div className={clsx('flex', 'mt-6')}>
+          <Button>Post your answer</Button>
+        </div>
+      </form>
     </div>
   )
 }
