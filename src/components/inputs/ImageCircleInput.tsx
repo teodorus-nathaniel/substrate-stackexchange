9597c748d57ce70f776995995230099f39efa83c
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-07-07 22:11:43 +0700

    enable set image as string to display in image circle input

--- src/components/inputs/ImageCircleInput.tsx
+++ src/components/inputs/ImageCircleInput.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { ChangeEvent, HTMLProps, useRef, useState } from 'react'
+import { ChangeEvent, HTMLProps, useEffect, useRef, useState } from 'react'
 import { BsArrowRepeat, BsImage } from 'react-icons/bs'
 import Button from '../Button'
 import ImageContainer from '../ImageContainer'
@@ -12,27 +12,49 @@ type ParentProps = Omit<HTMLProps<HTMLInputElement>, 'onChange'> &
   RequiredFieldWrapperProps
 export interface ImageCircleInputProps extends ParentProps {
   onChange?: (e: ChangeEvent<HTMLInputElement>, file: File) => void
+  image?: string | File
   imageContainerClassName?: string
+  boundAllContent?: boolean
 }
 
 export default function ImageCircleInput({
   imageContainerClassName,
+  image,
+  boundAllContent,
   ...props
 }: ImageCircleInputProps) {
   const inputRef = useRef<HTMLInputElement>(null)
   const [imageUrl, setImageUrl] = useState('')
 
+  useEffect(() => {
+    let url
+    if (typeof image !== 'string' && image) {
+      url = URL.createObjectURL(image)
+    } else {
+      url = image
+    }
+    setImageUrl(url ?? '')
+  }, [image])
+
   return (
     <FieldWrapper
       {...props}
-      containerClassName={clsx('items-center', props.containerClassName)}
+      containerClassName={clsx(
+        'items-center',
+        boundAllContent && 'w-32',
+        props.containerClassName
+      )}
+      helperTextClassName={clsx(
+        boundAllContent && 'text-center',
+        props.helperTextClassName
+      )}
     >
       {(id) => (
         <Button
           className={clsx(
             'bg-bg-200',
             'w-32 h-32',
-            'relative group',
+            'relative group mx-auto',
             imageContainerClassName
           )}
           rounded
@@ -48,8 +70,10 @@ export default function ImageCircleInput({
             onChange={(e) => {
               const files = e.target.files || []
               const file = files[0]
-              setImageUrl(URL.createObjectURL(file))
-              props.onChange && props.onChange(e, file)
+              if (file) {
+                setImageUrl(URL.createObjectURL(file))
+                props.onChange && props.onChange(e, file)
+              }
             }}
             className={clsx('hidden', props.className)}
             ref={inputRef}
