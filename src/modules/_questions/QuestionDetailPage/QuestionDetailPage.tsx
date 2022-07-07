21ebff780300ad73f541061ea7fd961969de7b92
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-07-07 22:33:57 +0700

    Remove dummy right section in question detail page

--- src/modules/_questions/QuestionDetailPage/QuestionDetailPage.tsx
+++ src/modules/_questions/QuestionDetailPage/QuestionDetailPage.tsx
@@ -1,12 +1,14 @@
-import TwoColumnsPageLayout from '#/containers/layouts/TwoColumnsPageLayout'
 import QuestionDetailSection from './QuestionDetailSection'
 
 export default function QuestionDetailPage() {
   return (
-    <TwoColumnsPageLayout
-      leftSection={<QuestionDetailSection />}
-      rightSection={<div>RIGHT SECTION</div>}
-      rightSectionSize='xs'
-    />
+    <div className='max-w-screen-md'>
+      <QuestionDetailSection />
+    </div>
+    // <TwoColumnsPageLayout
+    //   leftSection={<QuestionDetailSection />}
+    //   rightSection={<div>RIGHT SECTION</div>}
+    //   rightSectionSize='xs'
+    // />
   )
 }
