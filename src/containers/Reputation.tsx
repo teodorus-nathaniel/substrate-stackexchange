6d812fb88ebc9a454b73ed7e3a1850f99b1aaaf8
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-01-31 23:51:11 +0700

    Remove reputation functionality

--- src/containers/Reputation.tsx
+++ src/containers/Reputation.tsx
@@ -1,37 +1,27 @@
-import SkeletonFallback, {
-  useIntegratedSkeleton,
-} from '#/components/SkeletonFallback'
-import { encodeAddress } from '#/lib/helpers/chain'
-import { useGetReputationByAddress } from '#/services/indexing/queries'
-import clsx from 'clsx'
 import { HTMLProps } from 'react'
-import { BsAward } from 'react-icons/bs'
 
 export interface ReputationProps extends HTMLProps<HTMLDivElement> {
   address: string
 }
 
-export default function Reputation({
-  className,
-  address,
-  ...props
-}: ReputationProps) {
-  const { data, isLoading, isFetched } = useGetReputationByAddress(
-    encodeAddress(address)
-  )
-  const { loadingChecker, getContent } = useIntegratedSkeleton(
-    isLoading,
-    isFetched
-  )
+export default function Reputation(_props: ReputationProps) {
+  return null
+  // const { data, isLoading, isFetched } = useGetReputationByAddress(
+  //   encodeAddress(address)
+  // )
+  // const { loadingChecker, getContent } = useIntegratedSkeleton(
+  //   isLoading,
+  //   isFetched
+  // )
 
-  const reputation = data?.reputation?.value
+  // const reputation = data?.reputation?.value
 
-  return (
-    <div className={clsx('flex items-center', className)} {...props}>
-      <SkeletonFallback isLoading={loadingChecker(reputation)} width={50}>
-        <BsAward className='mr-1' />
-        <span>{getContent(reputation, 0)}</span>
-      </SkeletonFallback>
-    </div>
-  )
+  // return (
+  //   <div className={clsx('flex items-center', className)} {...props}>
+  //     <SkeletonFallback isLoading={loadingChecker(reputation)} width={50}>
+  //       <BsAward className='mr-1' />
+  //       <span>{getContent(reputation, 0)}</span>
+  //     </SkeletonFallback>
+  //   </div>
+  // )
 }
